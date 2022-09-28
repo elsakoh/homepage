74da4eefe96a067e75241ad421136dbb558954bd
elsa <elsayeahx3@gmail.com>
2022-09-28 10:28:51 +0800

    fix: add reference to original creator

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,16 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Link } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Takuya Matsuyama. All Rights Reserved.
+      &copy; {new Date().getFullYear()} Elsa Koh. All Rights Reserved.
+      <p>
+        This website is built based on{' '}
+        <Link href="https://www.craftz.dog/" target="_blank" rel="noreferrer">
+          Takuya Matsuyama's website
+        </Link>
+        .
+      </p>
     </Box>
   )
 }
